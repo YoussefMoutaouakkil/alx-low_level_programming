6d306cc5899f61df7eb9b2558fb17e95c0935b18
Youssef Moutaouakkil <youssef.moutaouakkil12@gmail.com>
2023-07-14 17:17:52 -0700

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Description: Positive anything is better than negative nothing
+*
+* Return: 0 (success)
+*
+*/
+
+int main(void)
+{
+	int n,digit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	digit = n % 10;
+	if (digit > 5)
+		printf("Last digit of %d is %d and is greater than 5\n",n,digit);
+	else if (digit == 0)
+		printf("Last digit of %d is %d and is 0\n",n,digit);
+	else if (digit < 6)
+		printf("Last digit of %d is %d and is less than 5\n",n,digit);
+	return (0);
+}
