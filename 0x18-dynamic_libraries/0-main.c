e43d69a0f8470b26c171d4b11ff97841c3a26f2e
Youssef Moutaouakkil <youssef.moutaouakkil12@gmail.com>
2024-01-16 00:52:24 +0100

    0x18-dynamic_libraries

--- 0x18-dynamic_libraries/0-main.c
+++ 0x18-dynamic_libraries/0-main.c
@@ -1,5 +1,20 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
+/**
+ * _strlen - Returns the lenght of a string.
+ * @s: Type char pointer
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != 0; c++)
+	{
+	}
+		return (c);
+
+}
 
 /**
  * main - check the code
